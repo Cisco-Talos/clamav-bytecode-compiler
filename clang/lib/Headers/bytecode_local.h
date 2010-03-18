471f4517877ff3c6f8343e5fb1dfb37b66b788f6
Török Edvin <edwin@clamav.net>
2010-03-18 17:53:00 +0200

    tempocommit.

--- clang/lib/Headers/bytecode_local.h
+++ clang/lib/Headers/bytecode_local.h
@@ -399,6 +399,7 @@ do {\
   uint32_t d = re2c_slim - re2c_scur;\
   uint32_t end = here - d;\
   unsigned len = end - re2c_stokstart;\
+  debug(len);\
   if (len > 80) {\
     unsigned skipped = len - 74;\
     seek(re2c_stokstart, SEEK_SET);\
@@ -448,6 +449,7 @@ do {\
       re2c_sres = 1;\
     } else {\
       re2c_slim += got;\
+      re2c_sres = -1;\
     }\
   }\
   else if (re2c_scur + len > re2c_seof)\
--- examples/in/pdf2.c
+++ examples/in/pdf2.c
@@ -31,19 +31,52 @@ int entrypoint(void)
   for (;;) {
     REGEX_LOOP_BEGIN
   /*!re2c
-
+    
     EOL = "\r" | "\n" | "\r\n";
     SKIPNOTEOL = [^\r\n]?;
     WS = [\000\t\r\h\n ];
     COMMENT = "%" SKIPNOTEOL EOL;
     WHITESPACE = (WS | COMMENT)+;
+    POSNUMBER = [0-9]+;
     ANY = [^];
 
-    POSNUMBER = [0-9]+;
+    DICT = "<<";
+
+    NAME_CHAR_J = "J" | "#4"[aA];
+    NAME_CHAR_S = ("S" | "#53");
+    NAME_CHAR_a = ("a" | "#61");
+    NAME_CHAR_c = ("c" | "#63");
+    NAME_CHAR_i = ("i" | "#69");
+    NAME_CHAR_p = ("p" | "#70");
+    NAME_CHAR_r = ("r" | "#72");
+    NAME_CHAR_t = ("t" | "#74");
+    NAME_CHAR_v = ("v" | "#76");
+
+    NAME_S = "/" NAME_CHAR_S;
+    NAME_JS = "/" NAME_CHAR_J NAME_CHAR_S;
+    NAME_JAVASCRIPT = "/" NAME_CHAR_J NAME_CHAR_a NAME_CHAR_v NAME_CHAR_a 
+                      NAME_CHAR_S NAME_CHAR_c NAME_CHAR_r NAME_CHAR_i 
+		      NAME_CHAR_p NAME_CHAR_t;
+
+    S_JAVASCRIPT = NAME_S WHITESPACE? NAME_JAVASCRIPT WHITESPACE?;
+    DIRECTJSOBJECT = DICT (WHITESPACE)? (S_JAVASCRIPT)? NAME_JS (WHITESPACE)?;
+    DIRECTTEXTJS = DIRECTJSOBJECT "(";
+    DIRECTHEXJS = DIRECTJSOBJECT "<";
+
+    INDIRECTPDFOBJECT = POSNUMBER WHITESPACE POSNUMBER WHITESPACE "R";
+    INDIRECTJSOBJECT = DICT WHITESPACE? S_JAVASCRIPT? NAME_JS WHITESPACE? INDIRECTPDFOBJECT;
+
     PDFOBJECT = POSNUMBER WHITESPACE POSNUMBER WHITESPACE "obj";
-    PDFOBJECT { debug("pdf obj at:"); DEBUG_PRINT_REGEX_MATCH; }
+    INDIRECTJS = NAME_JS WHITESPACE? INDIRECTPDFOBJECT;
+
+    DIRECTJSOBJECT { debug("pdfjs text at:"); DEBUG_PRINT_REGEX_MATCH; continue; }
+    DIRECTHEXJS { debug("pdfjs hextext at:"); DEBUG_PRINT_REGEX_MATCH; continue; }
+    INDIRECTJSOBJECT { debug("indirectjs at:"); DEBUG_PRINT_REGEX_MATCH; continue; }
     ANY { continue; }
   */
+#if 0
+    PDFOBJECT { debug("pdf obj at:"); DEBUG_PRINT_REGEX_MATCH; continue; }
+#endif
   }
   return REGEX_RESULT;
 }
