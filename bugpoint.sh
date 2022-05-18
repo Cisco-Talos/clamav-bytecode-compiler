e65e694820a592f9b9b7ad8e17fddf282f0114f9
Micah Snyder <micasnyd@cisco.com>
2022-05-18 13:36:07 -0700

    Remove legacy scripts not used after overhaul

--- bugpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm _bugpoint.bc
-obj/Release+Checks/bin/clambc-compiler -emit-llvm-bc -o _bugpoint.bc $@
-echo "exit 145" >_bugpoint.ref
-obj/Release+Checks/bin/bugpoint _bugpoint.bc -run-custom -safe-run-custom -append-exit-code -exec-command=./clamexec.sh -output=_bugpoint.ref
