bd4c08c31cae011448f9888f7c35ce3b9ea351cc
Micah Snyder <micasnyd@cisco.com>
2022-06-03 17:38:50 -0700

    Test: add test for COPYRIGHT() function
    Adds a test to verify that use of the COPYRIGHT() function in bytecode
    source correctly excludes the bytecode source from the compiled bytecode
    signature.
    
    I also tidied up the basic runtime tests to:
    - put test files in the test temp directory rather than the current
    directory.
    - make the variable names more Pythonic.
    - use pathlib.Path features rather than os.path.join().

---
+++ test/examples/in/lsig_copyright.c
@@ -0,0 +1,29 @@
+COPYRIGHT("Cisco 2022")
+
+VIRUSNAME_PREFIX("Clamav-Unit-Test-Signature.02")
+VIRUSNAMES("")
+TARGET(0)
+
+FUNCTIONALITY_LEVEL_MIN(FUNC_LEVEL_096_4)
+
+SIGNATURES_DECL_BEGIN
+DECLARE_SIGNATURE(test_string)
+SIGNATURES_DECL_END
+
+SIGNATURES_DEF_BEGIN
+/*   matches "CLAMAV-TEST-STRING-NOT-EICAR" */
+DEFINE_SIGNATURE(test_string, "0:434c414d41562d544553542d535452494e472d4e4f542d4549434152")
+SIGNATURES_DEF_END
+
+bool logical_trigger()
+{
+    /***Will return true if signature matches ***/
+    return matches(Signatures.test_string);
+}
+
+/***bytecode function that executes if the logical signature matched ***/
+int entrypoint(void)
+{
+    foundVirus("");
+    return 0;
+}
